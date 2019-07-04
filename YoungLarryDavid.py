ef19cca99eae867d284cec6ad3bea3bce27ac099
Dillon Calligy <dcalligy@mail.usf.edu>
2019-07-04 12:55:57 -0400

    Update YoungLarryDavid.py

--- YoungLarryDavid.py
+++ YoungLarryDavid.py
@@ -121,104 +121,6 @@ async def on_member_join(member):
     "For a list of all available commands, use the +help command.\n"
     "ps - Wu-Tang is 4 da children and don't forget to Protect Ya Kneck.")
 
-@client.event
-async def on_message(message):
-    # Don't need the bot to reply to itself.
-    if message.author == client.user:
-        return
-
-    # Randomly picks a lyric from the list of kanye_lyrics
-    if message.content.upper().startswith('+KANYE'):
-        await client.send_message(message.channel, random.choice(lyrics.kanye_lyrics))
-
-    # Randomly picks a lyric from the list of gucci_lyrics
-    if message.content.upper().startswith('+GUCCI'):
-        await client.send_message(message.channel, random.choice(lyrics.gucci_lyrics))
-
-    # Randomly picks a lyrics from the list of random_lyrics
-    if message.content.upper().startswith('+RANDOM'):
-        await client.send_message(message.channel, random.choice(lyrics.random_lyrics))
-
-    # Randomly picks lyrics from the list of nas_lyrics
-    # primarily lyrics from illmatic aka the best hip hop album of all time
-    if message.content.upper().startswith('+NAS'):
-        await client.send_message(message.channel, random.choice(lyrics.nas_lyrics))
-
-    # Randomly picks lyrics from the list of E40_lyrics
-    if message.content.upper().startswith('+E40'):
-        await client.send_message(message.channel, random.choice(lyrics.E40_lyrics))
-
-    # Randomly picks lyrics from the list of snoop_dogg_lyrics
-    if message.content.upper().startswith('+SNOOP'):
-        await client.send_message(message.channel, random.choice(lyrics.snoop_dogg_lyrics))
-
-    # Randomly picks lyrics from the list of three_six_lyrics
-    if message.content.upper().startswith('+TRIPLE6'):
-        await client.send_message(message.channel, random.choice(lyrics.three_six_lyrics))
-
-    # Randomly picks lyrics from the list of project_pat_lyrics
-    if message.content.upper().startswith('+PAT'):
-        await client.send_message(message.channel, random.choice(lyrics.project_pat_lyrics))
-
-    # Randomly picks lyrics from the list of wu_tang_lyrics
-    if message.content.upper().startswith('+WUTANG'):
-        await client.send_message(message.channel, random.choice(lyrics.wu_tang_lyrics))
-
-    # Randomly picks lyrics from the list of biggie_lyrics
-    if message.content.upper().startswith('+BIGGIE'):
-        await client.send_message(message.channel, random.choice(lyrics.biggie_lyrics))
-
-    # Rancomly picks lyrics from the list of doc_oct_lyrics
-    if message.content.upper().startswith('+DROCTAGON'):
-        await client.send_message(message.channel, random.choice(lyrics.doc_oct_lyrics))
-
-    # Randomly picks lyrics from the list of eminem_lyrics
-    if message.content.upper().startswith('+EMINEM'):
-        await client.send_message(message.channel, random.choice(lyrics.eminem_lyrics))
-
-    # Randomly picks lyrics from the list of gangsta gibbs lyrics
-    if message.content.upper().startswith('+GIBBS'):
-        await client.send_message(message.channel, random.choice(lyrics.freddie_gibbs_lyrics))
-
-    # Randomly picks lyrics from the list of Big L lyrics
-    if message.content.upper().startswith('+BIGL'):
-        await client.send_message(message.channel, random.choice(lyrics.big_L_lyrics))
-
-    # Randomly picks lyrics from the list of Outkast lyrics
-    if message.content.upper().startswith('+OUTKAST'):
-        await client.send_message(message.channel, random.choice(lyrics.outkast_lyrics))
-
-    # Displays the bots personal opinion on who are the top 10 best hip-hop artist of all time
-    if message.content.upper().startswith('+TOP10'):
-        await client.send_message(message.channel, "This is my top ten list of the best hip-hop artist of all time.\n"
-                                  "1. Nas\n"
-                                  "2. Ghostface Killah\n"
-                                  "3. Andre 3000\n"
-                                  "4. The Notorious B.I.G.\n"
-                                  "5. Big L\n"
-                                  "6. Raekwon da Chef\n"
-                                  "7. Tupac\n"
-                                  "8. Kendrick Lamar\n"
-                                  "9. Eminem\n"
-                                  "10. Jay-Z")
-
-    # Displays the bots personal opinion on who are the top 10 best hip-hop producers of all time
-    if message.content.upper().startswith('+PRODUCERS'):
-        await client.send_message(message.channel, "This is my top ten list of best hip-hop producers of all time.\n"
-                                  "1. J Dilla\n"
-                                  "2. Madlib\n"
-                                  "3. RZA\n"
-                                  "4. Dr. Dre\n"
-                                  "5. Organized Noize\n"
-                                  "6. No I.D.\n"
-                                  "7. Pete Rock\n"
-                                  "8. Sounwave\n"
-                                  "9. Q-Tip\n"
-                                  "10. Kanye West")
-
-    if message.content.upper().startswith('+SPIT'):
-        await client.send_message(message.channel, spit_game())
-
 @client.command(pass_context=True)
 async def help(ctx):
     author=ctx.message.author
@@ -228,25 +130,25 @@ async def help(ctx):
     )
     embed.set_author(name='Help')
     
-    embed.add_field(name="+kanye", value="Displays random lyrics from the greatest artist of our generation.", inline=False)
-    embed.add_field(name="+gucci", value="Displays lyrics by Guwop AKA El Gato the Human Glacier.", inline=False)
-    embed.add_field(name="+nas", value="Displays random lyrics from the greatest album of all time, Illmatic.", inline=False)
-    embed.add_field(name="+e40", value="Displays random lyrics by E40 AKA Charlie Hustle.", inline=False)
-    embed.add_field(name="+snoop", value="Displays random lyrics by the Dogg Father.", inline=False)
-    embed.add_field(name="+triple6", value="Displays random lyrics by Three 6 Mafia.", inline=False)
-    embed.add_field(name="+pat", value="Displays random lyrics by Project Pat.", inline=False)
-    embed.add_field(name="+wutang", value="Wu-Tang is for the the children.", inline=False)
-    embed.add_field(name="+bigge", value="Displays random lyrics by the black Frank White.", inline=False)
-    embed.add_field(name="+droctagon", value="Displays random lyrics by Dr.Octagon AKA the Dr.Octagonecologyst.", inline=False)
-    embed.add_field(name="+eminem", value="Displays random lyrics by Eminem.", inline=False)
-    embed.add_field(name="+gibbs", value="Displays random lyrics by Gangsta Gibbs.", inline=False)
-    embed.add_field(name="+bigl", value="Displays random lyrics by Big L.", inline=False)
-    embed.add_field(name="+outkast", value="Displays random lyrics by Outkast.", inline=False)
-    embed.add_field(name="+top10", value="Young LD displays his top 10 list of the best Hip-Hop artist of all time.", inline=False)
-    embed.add_field(name="+producers", value="Young LD displays his top 10 list of the best Hip-Hop producers of all time.", inline=False)
-    embed.add_field(name="+random", value="Displays random lyrics from a bunch of different artist.", inline=False)
-    embed.add_field(name="+spit", value="Uses a Markov chain to combine lyrics and comes up with some funky shit.", inline=False)
-    embed.add_field(name="+help", value="Young LD Displays the list of command that can be used.", inline=False)
+    embed.add_field(name='+kanye', value='Displays random lyrics from the greatest artist of our generation.', inline=False)
+    embed.add_field(name='+gucci', value='Displays lyrics by Guwop AKA El Gato the Human Glacier.', inline=False)
+    embed.add_field(name='+nas', value='Displays random lyrics from the greatest album of all time, Illmatic.', inline=False)
+    embed.add_field(name='+e40', value='Displays random lyrics by E40 AKA Charlie Hustle.', inline=False)
+    embed.add_field(name='+snoop', value='Displays random lyrics by the Dogg Father.', inline=False)
+    embed.add_field(name='+triple6', value='Displays random lyrics by Three 6 Mafia.', inline=False)
+    embed.add_field(name='+pat', value='Displays random lyrics by Project Pat.', inline=False)
+    embed.add_field(name='+wutang', value='Wu-Tang is for the the children.', inline=False)
+    embed.add_field(name='+bigge', value='Displays random lyrics by the black Frank White.', inline=False)
+    embed.add_field(name='+droctagon', value='Displays random lyrics by Dr.Octagon AKA the Dr.Octagonecologyst.', inline=False)
+    embed.add_field(name='+eminem', value='Displays random lyrics by Eminem.', inline=False)
+    embed.add_field(name='+gibbs', value='Displays random lyrics by Gangsta Gibbs.', inline=False)
+    embed.add_field(name='+bigl', value='Displays random lyrics by Big L.', inline=False)
+    embed.add_field(name='+outkast', value='Displays random lyrics by Outkast.', inline=False)
+    embed.add_field(name='+top10', value='Young LD displays his top 10 list of the best Hip-Hop artist of all time.', inline=False)
+    embed.add_field(name='+producers', value='Young LD displays his top 10 list of the best Hip-Hop producers of all time.', inline=False)
+    embed.add_field(name='+random', value='Displays random lyrics from a bunch of different artist.', inline=False)
+    embed.add_field(name='+spit', value='Uses a Markov chain to combine lyrics and comes up with some funky shit.', inline=False)
+    embed.add_field(name='+help', value='Young LD Displays the list of command that can be used.', inline=False)
     
     await client.send_message(author, embed=embed)
 
