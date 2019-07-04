c6a484e47d8b7465c8c776c3c2439c0868046358
Dillon Calligy <dcalligy@mail.usf.edu>
2019-07-04 13:06:09 -0400

    Update YoungLarryDavid.py

--- YoungLarryDavid.py
+++ YoungLarryDavid.py
@@ -221,12 +221,10 @@ async def on_message(message):
 
 @client.command(pass_context=True)
 async def help(ctx):
-    author=ctx.message.channel
-    
+
     embed=discord.Embed(
         color=discord.Color.orange()
     )
-    embed.set_author(name='Help')
     
     embed.add_field(name='+kanye', value='Displays random lyrics from the greatest artist of our generation.', inline=True)
     embed.add_field(name='+gucci', value='Displays lyrics by Guwop AKA El Gato the Human Glacier.', inline=True)
@@ -248,7 +246,7 @@ async def help(ctx):
     embed.add_field(name='+spit', value='Uses a Markov chain to combine lyrics and comes up with some funky shit.', inline=True)
     embed.add_field(name='+help', value='Young LD Displays the list of command that can be used.', inline=True)
     
-    await client.send_message(embed=embed)
+    await ctx.send_message(embed=embed)
 
 async def list_server():
     await client.wait_until_ready()
