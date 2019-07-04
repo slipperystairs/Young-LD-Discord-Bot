5423795e1487f2e041bc0d59b5a7e36fcac988e3
Dillon Calligy <dcalligy@mail.usf.edu>
2019-07-04 13:15:38 -0400

    Update YoungLarryDavid.py

--- YoungLarryDavid.py
+++ YoungLarryDavid.py
@@ -221,7 +221,7 @@ async def on_message(message):
 
 @client.command(pass_context=True)
 async def help(ctx):
-    author = ctx.message.channel
+    author = ctx.message.author
     embed=discord.Embed(
         color=discord.Color.orange()
     )
@@ -247,7 +247,7 @@ async def help(ctx):
     embed.add_field(name='+spit', value='Uses a Markov chain to combine lyrics and comes up with some funky shit.', inline=True)
     embed.add_field(name='+help', value='Young LD Displays the list of command that can be used.', inline=True)
     
-    await client.send_message(author,embed=embed)
+    await client.send_message(author.channel,embed=embed)
 
 async def list_server():
     await client.wait_until_ready()
