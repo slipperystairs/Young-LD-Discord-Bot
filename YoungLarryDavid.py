674cdf2e711a815c3972fe45ae69c23709bdca0b
Dillon Calligy <dcalligy@mail.usf.edu>
2019-07-04 20:18:44 -0400

    Update YoungLarryDavid.py

--- YoungLarryDavid.py
+++ YoungLarryDavid.py
@@ -251,8 +251,22 @@ async def on_message(message):
 """
 @client.command(pass_context=True)
 async def kanye(ctx):
-
-    await client.say(lyrics.kanye_lyrics)
+    kanye_lyrics = random.choice([
+    'The same people that tried to blackball me forgot about two things: my black balls',
+    'What’s a black Beatle anyway? A fucking roach? I guess that’s why they got me sitting in fucking coach',
+    'Have you ever had sex with a pharaoh? Put the pussy in a sarcophagus. Now she claiming that I bruised her esophagus',
+    'My apologies, are you into astrology. Cause I’m tryin’ to make it to Uranus',
+    'I keep it 300 like the romans',
+    'They be ballin in the D-League. I be speaking Swaghili',
+    'And if life’s a bitch, bet she suck my dick, huh I bet she fucked the whole clique, huh',
+    'Eatin Asian pussy, all I need was sweet and sour sauce',
+    'I just talked to Jesus. He said “What up, Yeezus?!?” I said “Shit, I’m chillin tryna stack these millions',
+    'Mayonnaise-colored Benz, I push Miracle Whips'
+    'Now, if I fuck this model.\n' + 'And she just bleached her asshole\n' +
+    'And I get bleach on my T-shirt\n' + 'I\'ma feel like an asshole'
+    ])
+    
+    await client.say(kanye_lyrics)
 
 async def list_server():
     await client.wait_until_ready()
