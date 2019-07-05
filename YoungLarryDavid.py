dcbdf86ba92d88f01a7af475f2e9584a7ea38cd8
Dillon Calligy <dcalligy@mail.usf.edu>
2019-07-04 20:09:16 -0400

    Update YoungLarryDavid.py

--- YoungLarryDavid.py
+++ YoungLarryDavid.py
@@ -120,7 +120,7 @@ async def on_member_join(member):
     await client.send_message(member, "Sup!? It's ya boy Young LD, and my sole purpose in this world is to provide you and your crew with some dank, absurd, hard hittin' rap lyrics.\n" 
     "For a list of all available commands, use the +help command.\n"
     "ps - Wu-Tang is 4 da children and don't forget to Protect Ya Kneck.")
-    
+"""  
 @client.event
 async def on_message(message):
     # Don't need the bot to reply to itself.
@@ -248,27 +248,12 @@ async def on_message(message):
             msg.add_field(name=command, value=description, inline=False)
         #msg.add_field(name='Join our Discord/For Questions/Chilling', value='', inline=False)
         await client.send_message(message.channel, embed=msg)
-
 """
-Will add command functions later
-@client.command(pass_context=True,)
+@client.command(pass_context=True)
 async def kanye(ctx):
-    kanye_lyrics = random.choice ([
-    'The same people that tried to blackball me forgot about two things: my black balls',
-    'What’s a black Beatle anyway? A fucking roach? I guess that’s why they got me sitting in fucking coach',
-    'Have you ever had sex with a pharaoh? Put the pussy in a sarcophagus. Now she claiming that I bruised her esophagus',
-    'My apologies, are you into astrology. Cause I’m tryin’ to make it to Uranus',
-    'I keep it 300 like the romans',
-    'They be ballin in the D-League. I be speaking Swaghili',
-    'And if life’s a bitch, bet she suck my dick, huh I bet she fucked the whole clique, huh',
-    'Eatin Asian pussy, all I need was sweet and sour sauce',
-    'I just talked to Jesus. He said “What up, Yeezus?!?” I said “Shit, I’m chillin tryna stack these millions',
-    'Mayonnaise-colored Benz, I push Miracle Whips'
-    'Now, if I fuck this model.\n' + 'And she just bleached her asshole\n' +
-    'And I get bleach on my T-shirt\n' + 'I\'ma feel like an asshole'
-    ])
-    await client.say(kanye_lyrics)
-"""
+
+    await client.say(lyrics.kanye_lyrics)
+
 async def list_server():
     await client.wait_until_ready()
     while not client.is_closed:
