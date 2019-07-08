f7c54ebd8358f6b71b471bb1604fe2c4831cb22e
Dillon Calligy <dcalligy@mail.usf.edu>
2019-07-08 18:30:31 -0400

    Update YoungLarryDavid.py

--- YoungLarryDavid.py
+++ YoungLarryDavid.py
@@ -16,39 +16,12 @@ BOT_PREFIX = ("+")
 
 client = Bot(command_prefix=BOT_PREFIX)
 #client.remove_command('help') will fix this later
-#extensions  = ['ld']
 
 @client.event
 async def on_ready():
     print("Young LD is online")
     init_markov()
     await client.change_presence(game=discord.Game(name='+help for command list!'))
-
-"""
-@client.command()
-async def load(extension):
-    try:
-        client.load_extension(extension)
-        print('Loaded {}'.format(extension))
-    except Exception as error:
-        print('{} cannot be loaded. [{}]'.format(extension, error))
-
-@client.command()
-async def unload(extension):
-    try:
-        client.unload_extension(extension)
-        print('Unloaded {}'.format(extension))
-    except Exception as error:
-        print('{} cannot be unloaded. [{}]'.format(extension, error))
-
-if __name__ == '__main__':
-    for extension in extensions:
-        try:
-            client.load_extension(extension)
-        except Exception as error:
-            print('{} cannot be loaded. [{}]'.format(extension, error))
-    client.run(os.getenv('TOKEN'))
-"""
 
 # Sends a message to a new member that joins the discord server.
 @client.event
